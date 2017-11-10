f2549b793d2ee8be7e88085ca01d14fc7668475d
Ricardo de Souza Moura <ricardo.moura@petrobras.com.br>
2017-11-10 09:25:21 -0200

    ajustando as configs do mongodb

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
 
     getDbConnectionString: function(){
         //return "mongodb://"+configFiles.usr+":"+configFiles.pwd+"@10.24.144.156:27017/test";
-        //return "mongodb://10.24.144.156/ForumTecnico";
-        return 'mongodb://'+configValues.usr + ':' + configValues.pwd + '@ds113785.mlab.com:13785/forumtecnicosmp3';
+        return "mongodb://10.24.144.156/ForumTecnico";
+        //return 'mongodb://'+configValues.usr + ':' + configValues.pwd + '@ds113785.mlab.com:13785/forumtecnicosmp3';
     }
 
 }
